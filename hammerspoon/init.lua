af75abb33371e12b9c02196a77ed7a7dd35df850
nnarayen <nnarayen@berkeley.edu>
2019-05-30 00:16:37 -0400

    Update init.lua

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -1,10 +1,11 @@
 -- name = "Nikhil Narayen"
--- email = "nikhil.narayen@airbnb.com"
+-- email = "nikhil.narayen@gmail.com"
 
 -- Define default Spoons which will be loaded later
 if not hspoon_list then
   hspoon_list = {
     "WinWin",
+    "ReloadConfiguration",
   }
 end
 
@@ -16,35 +17,28 @@ end
 -- Remove animation delay
 hs.window.animationDuration = 0
 
-homeDir = os.getenv("HOME")
-function bindAppToKey(useHomeAppPath, key, app)
-  local appPath
-
-  if useHomeAppPath == true then
-    appPath = homeDir .. "/Applications/" .. app .. ".app"
-  else
-    appPath = app
-  end
-
-  hs.hotkey.bind(mash, key, function() hs.application.launchOrFocus(appPath) end)
-end
-
 -- Custom hyper keys
 mash = {"cmd", "shift"}
 
 -- Apps
-bindAppToKey(false, "c", "Google Chrome")
-bindAppToKey(false, "s", "Slack")
-bindAppToKey(false, "i", "iTerm")
-bindAppToKey(false, "e", "Messages")
-bindAppToKey(false, "o", "Notes")
-bindAppToKey(false, "1", "1Password 6")
-bindAppToKey(false, "d", "IntelliJ IDEA")
+local apps = {
+  c = 'Google Chrome',
+  s = 'Slack',
+  i = 'iTerm',
+  e = 'Messages',
+  o = 'Notes',
+  d = 'IntelliJ IDEA'
+}
+apps['1'] = '1Password 7'
+
+for key, app in pairs(apps) do
+  hs.hotkey.bind(mash, key, function() hs.application.launchOrFocus(app) end)
+end
 
 -- Put mac to sleep
 hs.hotkey.bind(mash, "space", function() hs.caffeinate.systemSleep() end)
 
---- Move window to left/right/top/bottom half of the screen
+-- Move window to left/right/top/bottom half of the screen
 hs.hotkey.bind(mash, "h", function() spoon.WinWin:moveAndResize("halfleft") end)
 hs.hotkey.bind(mash, "l", function() spoon.WinWin:moveAndResize("halfright") end)
 hs.hotkey.bind(mash, "k", function() spoon.WinWin:moveAndResize("halfup") end)
@@ -53,26 +47,14 @@ hs.hotkey.bind(mash, "j", function() spoon.WinWin:moveAndResize("halfdown") end)
 -- Maximize screen
 hs.hotkey.bind(mash, "m", function() spoon.WinWin:moveAndResize("fullscreen") end)
 
---- Multiple Monitor Actions
+-- Multiple Monitor Actions
 hs.hotkey.bind(mash, "p", function()
   local win = hs.window.focusedWindow()
   win:moveToScreen(win:screen():previous())
 end)
 
 -- Reload config
-function reloadConfig(files)
-  doReload = false
-  for _, file in pairs(files) do
-    if file:sub(-4) == ".lua" then
-      doReload = true
-    end
-  end
-  if doReload then
-    hs.reload()
-  end
-end
-hs.pathwatcher.new(homeDir .. "/.hammerspoon/", reloadConfig):start()
-hs.alert.show("Config loaded")
+spoon.ReloadConfiguration:start()
 
 -- Show window hints
 hs.hotkey.bind(mash, "a", function()
@@ -89,3 +71,6 @@ caffeinate_watcher = hs.caffeinate.watcher.new(function(state)
   end
 end)
 caffeinate_watcher:start()
+
+-- Loaded!
+hs.alert.show("Config loaded")
