37f5dd6e3b61beb232e9f51eb6362ad4d2860812
Nikhil Narayen <nikhil.narayen@gmail.com>
2025-01-06 22:46:51 -0500

    Update bootstrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,4 +5,20 @@ sudo apt install -y openssh-server
 sudo service ssh start
 
 # Install dependencies
-brew install neovim ripgrep fzf bat fd
+brew install neovim ripgrep fzf bat fd zoxide starship
+
+# Configure nvim
+pushd $(dirname $0)
+ln -f -s $PWD/vim ~/.config/nvim
+
+# Configure tmux
+ln -f -s $PWD/tmux/tmux.conf ~/.tmux.conf
+ln -f -s $PWD/tmux/tmux-statusline-colors.conf ~/.tmux-statusline-colors.conf
+
+# Configure zsh, aliases
+ln -f -s $PWD/zsh/zshrc $ZSH_CUSTOM/zshrc-custom.zsh
+ln -f -s $PWD/zsh/zsh_aliases $ZSH_CUSTOM/zshrc-aliases.zsh
+
+# Configure starship, custom directory gets overriden
+echo "eval \"\$(starship init zsh)\"" >> ~/.zshrc
+ln -f -s $PWD/starship/starship.toml ~/.config/starship.toml
