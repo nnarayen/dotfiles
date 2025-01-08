d6c3d86192d04921b639702b88b71a81fb570042
Nikhil Narayen <nikhil.narayen@gmail.com>
2025-01-08 06:18:50 -0500

    Update starship

--- bootstrap.sh
+++ bootstrap.sh
@@ -12,9 +12,14 @@ pushd $(dirname $0)
 ln -f -s $PWD/vim ~/.config/nvim
 
 # Configure tmux, brew version is required for clipboard functionality
-sudo apt-get remove tmux
+sudo apt-get remove -y tmux
 ln -f -s $PWD/tmux/tmux.conf ~/.tmux.conf
 ln -f -s $PWD/tmux/tmux-statusline-colors.conf ~/.tmux-statusline-colors.conf
+
+# Download tpm, install plugins
+mkdir -p ~/.tmux/plugins
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+~/.tmux/plugins/tpm/bin/install_plugins
 
 # Configure zsh, aliases
 ln -f -s $PWD/zsh/zshrc $ZSH_CUSTOM/zshrc-custom.zsh
